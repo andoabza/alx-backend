0da38951c94ad2d3faf4d95d0736ad7013aac8f1
ando <terbu102@gmail.com>
2024-01-29 09:00:54 +0300

    fix pycode style

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -36,21 +36,23 @@ class Server:
         if start > len(data):
             return []
         return data[start:end]
-    
+
     def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
         '''get values as dict'''
-        assert isinstance(page, int) and page > 0, "Page must be an integer greater than 0"
-        assert isinstance(page_size, int) and page_size > 0, "Page size must be an integer greater than 0"
+        assert isinstance(page, int) and page > 0, \
+            "Page must be an integer greater than 0"
+        assert isinstance(page_size, int) and page_size > 0, \
+            "Page size must be an integer greater than 0"
         total = len(self.dataset())
         pages = math.ceil(total / page_size)
         data = self.get_page(page, page_size)
         next_page = page + 1 if page < pages else None
         prev_page = page - 1 if page > 1 else None
         return {
-        'page_size': len(data),
-        'page': page,
-        'data': data,
-        'next_page': next_page,
-        'prev_page': prev_page,
-        'total_pages': pages
-    }
+            'page_size': len(data),
+            'page': page,
+            'data': data,
+            'next_page': next_page,
+            'prev_page': prev_page,
+            'total_pages': pages
+            }
