122f6391ed1a55a86a5c6ef422c459f4bad3d538
ando <terbu102@gmail.com>
2024-01-29 08:39:30 +0300

    fix typo

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -23,11 +23,12 @@ class Server:
 
         return self.__dataset
 
-    
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         '''return start and end'''
-        assert isinstance(page, int) and page > 0, "Page must be an integer greater than 0"
-        assert isinstance(page_size, int) and page_size > 0, "Page size must be an integer greater than 0"
+        assert isinstance(page, int) and page > 0, \
+            "Page must be an integer greater than 0"
+        assert isinstance(page_size, int) and page_size > 0, \
+            "Page size must be an integer greater than 0"
         start = (page - 1) * page_size
         end = page * page_size
         data = self.dataset()
