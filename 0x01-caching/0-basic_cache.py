5e7f2fa499ac8193ffae7416d7e13073a1cb2a51
ando <terbu102@gmail.com>
2024-01-31 22:29:31 +0300

    basic cache

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+'''a caching system'''
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    '''basic cache'''
+    def put(self, key, item):
+        '''method to update chache'''
+        if key != None or item != None:
+            self.cache_data.update({key: item})
+        return
+    
+    def get(self, key):
+        '''method to get by key'''
+        if key:
+           return self.cache_data.get(key)
+        return None
