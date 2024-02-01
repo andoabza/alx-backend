c99c75a9f90d1bb8274710be8fbd83910d1c9a82
ando <terbu102@gmail.com>
2024-02-01 11:16:32 +0300

    fifo algorithm

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+'''a caching system'''
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    '''first in first out cache'''
+    def __init__(self):
+        super().__init__()
+
+    def put(self, key, item):
+        if key and item:
+            if len(self.cache_data) >= self.MAX_ITEMS:
+                index = list(self.cache_data)[0]
+                if len(self.cache_data) == self.MAX_ITEMS:
+                    self.cache_data.pop(index)
+                    print(f"DISCARD: {index}")
+                    self.cache_data.update({key: item})
+                # self.cache_data.pop(index)
+                # print(f"DISCARD: {index}")
+            self.cache_data.update({key: item})
+
+        return
+
+    def get(self, key):
+        if key:
+            return self.cache_data.get(key)
+        return None
