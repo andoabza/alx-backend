616f3bf6a67ed50a5c16ded312dd0ca3e08f6872
ando <terbu102@gmail.com>
2024-02-01 11:46:06 +0300

    last recently used

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+'''a caching system'''
+from base_caching import BaseCaching
+
+
+class LRUCache(BaseCaching):
+    '''least recently used first out cache'''
+    def __init__(self):
+        super().__init__()
+
+    def put(self, key, item):
+        '''update the data'''
+        if key and item:
+            if len(self.cache_data) >= self.MAX_ITEMS:
+                index = next(iter(self.cache_data))
+                self.cache_data.pop(index)
+                print(f'DISCARD: {index}')
+            self.cache_data.update({key: item})
+        return
+
+    def get(self, key):
+        '''get value by key'''
+        if key:
+            return self.cache_data.get(key)
+        return None
