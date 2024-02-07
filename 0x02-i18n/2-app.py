f7cfabced3399ee1c82b8316f88731641b11dbc5
ando <terbu102@gmail.com>
2024-02-07 09:30:16 +0300

    fix pycode

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -4,15 +4,23 @@ from flask import request, Flask, render_template
 from flask_babel import Babel
 
 
+@Babel.localeselector
 def get_locale():
     '''get local language and time'''
     return request.accept_languages.best_match(app.config('LANGUAGES'))
 
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 babel = Babel(app, locale_selector=get_locale)
 
 
 @app.route('/')
-def index():
+def index() -> str:
+    '''home route'''
     return render_template('2-index.html')
+
+
+if __name__ == '__main__':
+    '''main'''
+    app.run()
