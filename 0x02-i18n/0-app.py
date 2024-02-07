70837c54cacd384e35195a83695a7c38c666ecdc
ando <terbu102@gmail.com>
2024-02-07 09:06:54 +0300

    fix strict slashes

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -4,9 +4,10 @@ from flask import Flask, render_template
 
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
 @app.route('/')
-def home():
+def home() -> str:
     '''index route'''
     return render_template('0-index.html')
