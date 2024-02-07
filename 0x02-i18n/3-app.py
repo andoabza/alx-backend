701e7febb83aa84eaa2a6504cbf2124e80a2cee4
ando <terbu102@gmail.com>
2024-02-07 10:32:36 +0300

    add route

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,19 +1,17 @@
 #!/usr/bin/env python3
 '''parametrize templates'''
-from flask_babel import Babel, gettext
+from flask_babel import Babel, gettext as _
 from flask import Flask, render_template
 
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
-app.config.from_pyfile('babel.cfg')
 babel = Babel(app)
 
 
 @app.route('/')
 def get_index() -> str:
     '''home route'''
-    gettext('home_title', 'home_header')
     return render_template('3-index.html')
 
 
