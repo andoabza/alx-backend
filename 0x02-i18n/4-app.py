71e5c1cd8e0616ecaf65d07c6a07a6a2c99a7689
ando <terbu102@gmail.com>
2024-02-07 11:08:47 +0300

    fix pycode

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,28 +1,38 @@
-'''get locale'''
-from flask import request, Flask, render_template
-from flask_babel import babel
+#!/usr/bin/env python3
+"""force locale by passing arg to flask app
+"""
+from flask import Flask, render_template, request, g
+from flask_babel import Babel
+
+
+class Config:
+    """config class"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
-babel.init_app(app)
+app.config.from_object(Config)
+babel = Babel(app)
 
 
 @babel.localeselector
-def get_locale() -> str:
-    '''get local language and time'''
-    local = request.args.get('locale')
-    support = ['en', 'fr']
-    if local in support:
+def get_locale():
+    """get locale"""
+    local = request.args.get("locale")
+    if local and local in app.config["LANGUAGES"]:
         return local
-    return ['en']
+    print(local)
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
 
 @app.route('/')
-def index() -> str:
-    '''home route'''
+def index():
+    """index page"""
     return render_template('4-index.html')
 
 
-if __name__ == '__main__':
-    '''main'''
+if __name__ == "__main__":
     app.run()
