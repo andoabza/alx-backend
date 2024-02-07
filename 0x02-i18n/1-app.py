20ce00fda163251017d6ab7836c8896be1d10b0c
ando <terbu102@gmail.com>
2024-02-07 09:24:48 +0300

    fix pycode

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -4,23 +4,21 @@ from flask import Flask, render_template
 from flask_babel import Babel
 
 
-app = Flask(__name__)
-app.url_map.strict_slashes = False
-
-
-class Config():
+class Config:
     '''config class'''
     LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
+app = Flask(__name__)
+app.url_map.strict_slashes = False
 app.config.from_object(Config)
-app.config['BABEL_DEFAULT_LOCALE'] = 'en'
-app.config['BABEL_DEFAULT_TIMEZONE'] = 'UTC'
 babel = Babel(app)
 
 
 @app.route('/')
-def get_index():
+def get_index() -> str:
     '''get home route'''
     return render_template('1-index.html')
 
