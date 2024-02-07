33a56406cc973fa69a83b2eaac25433f7b5b1a62
ando <terbu102@gmail.com>
2024-02-07 09:17:08 +0300

    fix strict slashes

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,8 +1,10 @@
-#!/usr/bin/env python3
 '''falsk app'''
 from flask import Flask, render_template
 from flask_babel import Babel
+
+
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
 class Config():
@@ -11,11 +13,17 @@ class Config():
 
 
 app.config.from_object(Config)
-app.config['BABEL_DEFAULT_LOCALE'] = 'en'
+app.config['BABEL_DEFAULT_LOCALE'] = 'fr'
 app.config['BABEL_DEFAULT_TIMEZONE'] = 'UTC'
 babel = Babel(app)
 
 
 @app.route('/')
-def index():
+def get_index():
+    '''get home route'''
     return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    '''main'''
+    app.run()
