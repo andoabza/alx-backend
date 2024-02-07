dc4986f05e55574452564eff593ddc41403830f1
ando <terbu102@gmail.com>
2024-02-07 10:20:40 +0300

    parameter template

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+'''parametrize templates'''
+from flask_babel import gettext, Babel
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+app.config.from_pyfile('babel.cfg')
+babel = Babel(app)
+
+
+@app.route('/')
+def get_index() -> str:
+    gettext('home_title', 'home_header')
+    return render_template('3-index.html')
+
+
+if __name__ == '__main__':
+    '''main'''
+    app.run()
