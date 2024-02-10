673c8f44ab7577bd4becd8049268e2cf92f26198
ando <terbu102@gmail.com>
2024-02-10 12:23:04 +0300

    config

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,41 +1,28 @@
 #!/usr/bin/env python3
-"""A Basic Flask app with internationalization support.
+"""force babel from arg
 """
-from flask_babel import Babel, gettext
+
 from flask import Flask, render_template, request
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
 
 
 class Config:
-    """Represents a Flask Babel configuration.
-    """
+    """Config app"""
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
-app = Flask(__name__)
 app.config.from_object(Config)
 
 
-@babel.localeselector
-def get_locale() -> str:
-    """Retrieves the locale for a web page.
-    """
-    query = request.args.get('locale')
-    if query in app.config["LANGUAGES"]:
-        return query
-    return request.accept_languages.best_match(app.config["LANGUAGES"])
-
-
-babel = Babel(app, locale_selector=get_locale)
-
-
 @app.route('/')
-def get_index() -> str:
-    """The home/index page.
-    """
-    return render_template('4-index.html', home_title=gettext('home_title'),
-                           home_header=gettext('home_header'))
+def index() -> str:
+    """index page"""
+    return render_template('4-index.html')
 
 
 if __name__ == '__main__':
