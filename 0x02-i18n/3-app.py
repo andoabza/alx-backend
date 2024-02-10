66bcde0e506f49616d8ad3c3e54cbf60c6b2904e
ando <terbu102@gmail.com>
2024-02-10 12:04:24 +0300

    py fix

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -8,12 +8,29 @@ app = Flask(__name__)
 babel = Babel(app)
 
 
+class Config(object):
+    '''config babel'''
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    '''get locale'''
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
 @app.route('/')
 def get_index():
     '''get index'''
     title = gettext('home_title')
     header = gettext('home_header')
-    return render_template('3-index.html', main_title=title, main_header=header)
+    return render_template('3-index.html',
+                           main_title=title, main_header=header)
 
 
 if __name__ == '__main__':
