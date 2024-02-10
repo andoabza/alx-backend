d991e12799a6585c64676146a0ea5e9816883e79
ando <terbu102@gmail.com>
2024-02-10 09:59:20 +0300

    babel local selector

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,21 +1,24 @@
-#!/usr/bin/env python3
-'''get locale'''
-from flask import request, Flask, render_template
+'''get locale with @babel.localeselector'''
+
+from flask import Flask, render_template, request
 from flask_babel import Babel
 
-
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 babel = Babel(app)
+
+
+class Config:
+    '''config class'''
+    LANGUAGES = ['en', 'fr']
+
+
+app.config.from_object(Config)
 
 
 @babel.localeselector
 def get_locale() -> str:
-    '''get local language and time'''
-    return request.accept_languages.best_match(app.config('LANGUAGES'))
-
-
-babel.init_app(app, locale_selector=get_locale)
+    '''get locale'''
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/')
