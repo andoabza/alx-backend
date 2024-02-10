298a717aa1f658c275f91ab86fa4093d5a9046f8
ando <terbu102@gmail.com>
2024-02-10 10:32:10 +0300

    config

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -6,11 +6,13 @@ from flask_babel import Babel, gettext as _
 app = Flask(__name__)
 babel = Babel(app)
 
+
 class Config:
     '''config babel'''
     LANGUAGES = ['en', 'es']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
+
 
 app.config.from_object(Config)
 
@@ -18,10 +20,7 @@ app.config.from_object(Config)
 @app.route('/')
 def get_index():
     '''get index'''
-    _home_title = _('home_title')
-    _home_header = _('home_header')
-    return render_template('3-index.html', home_title=_home_title,
-                           home_header=_home_header)
+    return render_template('3-index.html')
 
 
 if __name__ == '__main__':
