bc6cb6152b388abb327791885360c2b11c5148a3
ando <terbu102@gmail.com>
2024-02-10 12:05:30 +0300

    config fix

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -8,22 +8,6 @@ app = Flask(__name__)
 babel = Babel(app)
 
 
-class Config(object):
-    '''config babel'''
-    LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFAULT_TIMEZONE = "UTC"
-
-
-app.config.from_object(Config)
-
-
-@babel.localeselector
-def get_locale():
-    '''get locale'''
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
-
-
 @app.route('/')
 def get_index():
     '''get index'''
