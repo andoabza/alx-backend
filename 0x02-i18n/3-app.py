060212a4c1face12c971bde7d580e2d64179bbf9
ando <terbu102@gmail.com>
2024-02-10 12:01:32 +0300

    py babel

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,16 +1,20 @@
-#!/usr/bin/env python3
+
 '''parametrize templates by _ and gettext'''
 from flask import Flask, render_template, request
-from flask_babel import gettext as _
+from flask_babel import Babel, gettext
+import babel
 
 
 app = Flask(__name__)
+babel = Babel(app)
 
 
 @app.route('/')
 def get_index():
     '''get index'''
-    return render_template('3-index.html')
+    title = gettext('home_title')
+    header = gettext('home_header')
+    return render_template('3-index.html', main_title=title, main_header=header)
 
 
 if __name__ == '__main__':
