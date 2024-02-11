9b13a4d0fb2f99c32cd9efcea00bf70cd72fd7d7
ando <terbu102@gmail.com>
2024-02-11 22:52:25 +0300

    config

--- 0x02-i18n/6-app.py
+++ 0x02-i18n/6-app.py
@@ -45,9 +45,9 @@ def get_locale() -> str:
     if query in app.config('LANGUAGES'):
         return query
     user_setting = g.user['locale']
-    if user_setting and user_setting in app.config('LANGUAGES'):
+    if user_setting in app.config('LANGUAGES'):
         return user_setting
-    return request.accept_languages.best_match(Config)
+    return request.accept_languages.best_match(app.config('LANGUAGES'))
 
 
 @app.route('/')
