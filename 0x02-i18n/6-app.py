358402276c30268ea1227299f632d58f307b04e7
ando <terbu102@gmail.com>
2024-02-11 22:35:59 +0300

    g.user locale

--- 0x02-i18n/6-app.py
+++ 0x02-i18n/6-app.py
@@ -41,8 +41,8 @@ def before_request() -> None:
 
 @babel.localselector
 def get_locale() -> str:
-    query = request.args.get('locale')
-    if query and query in Config.LANGUAGES:
+    query = get_user['locale']
+    if query in app.config('LANGUAGES'):
         return query
     user_setting = g.user['locale']
     if user_setting and user_setting in Config.LANGUAGES:
