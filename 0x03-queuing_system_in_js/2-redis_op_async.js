15a67426244d6a3835a1171d501b50093a3accda
ando <terbu102@gmail.com>
2024-03-06 22:12:19 +0300

    async await

---
+++ 0x03-queuing_system_in_js/2-redis_op_async.js
@@ -0,0 +1,20 @@
+import { createClient } from 'redis';
+
+const client = createClient()
+    .on('err', err => console.log('Redis client not connected to the server:', err))
+    .on('connect', () => console.log('Redis client connected to the server'));
+
+const setNewSchool = (schoolName, value) => client.set(schoolName, value, (err, reply) => {
+    console.log(reply);
+});
+
+async function displaySchoolValue(schoolName) { 
+    await client.get(schoolName, (err, reply) => {
+    console.log(reply);
+});
+}
+
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
